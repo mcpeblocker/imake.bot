b33e687b38ff0196b6ae67cecd9854e9901ecfb0
Alisher Ortiqov <mcpeblocker@gmail.com>
2024-01-03 17:48:18 +0500

    007-Frontend-1: Linting error fixed: Naming anonymous function.

--- frontend/app/dashboard/page.tsx
+++ frontend/app/dashboard/page.tsx
@@ -1,7 +1,7 @@
 import { ChatBots } from "@/components/modules/chatbot/ChatBots";
 import { Container } from "@mui/joy";
 
-export default function () {
+export default function Page() {
   return (
     <Container component="main">
       {/* ChatBots list */}
