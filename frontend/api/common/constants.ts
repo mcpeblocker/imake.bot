bb78de60c90fbbb1b27f8ad635c596cd31af5416
Alisher Ortiqov <mcpeblocker@gmail.com>
2024-01-27 16:08:17 +0500

    Fronte-end api url config via env

--- frontend/api/common/constants.ts
+++ frontend/api/common/constants.ts
@@ -1,3 +1,4 @@
 // export const baseApiUrl = "http://backend:8000";
-export const baseApiUrl = "http://localhost:8000";
+// export const baseApiUrl = "http://localhost:8000";
+export const baseApiUrl = process.env.API_URL;
 export const userId = "658fb2785ba575f535d8382f";
